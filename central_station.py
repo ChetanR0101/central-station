c68b272e9c307d97113717c302f901fe7414cb99
ChetanR0101 <104768663+ChetanR0101@users.noreply.github.com>
2022-05-16 13:04:25 +0530

    Update central_station.py

--- central_station.py
+++ central_station.py
@@ -1,7 +1,7 @@
-from email.policy import default
 from xmlrpc.client import Boolean
 from odoo import models,fields,api
 from odoo.exceptions import ValidationError
+
 
 class CentralStation_substation(models.Model):
     _name= "central.substation"
@@ -10,16 +10,16 @@ class CentralStation_substation(models.Model):
     price=fields.Float("Fuel Price")
 
 
-class CentralStation_transection_in(models.Model):
-    _name= "central.transection_in"
+class CentralStation_transaction_in(models.Model):
+    _name= "central.transaction_in"
     name= fields.Char("Recived By")
     date= fields.Datetime("Date")
     fuel_type= fields.Many2one(string="Fuel Type", comodel_name="central.data")
     sub_station=fields.Many2one(string="SubStation Name", comodel_name="central.substation")
-    instock_qut=fields.Float("InStock Qut")
+    instock_qut=fields.Float("Fuel Quantity")
 
-class CentralStation_transection_out(models.Model):
-    _name= "central.transection_out"
+class CentralStation_transaction_out(models.Model):
+    _name= "central.transaction_out"
     name=fields.Char("Customer Name")
     date=fields.Datetime("Date")
     fuel_type=fields.Many2one(string="Fuel Type", comodel_name="central.data")
@@ -39,25 +39,45 @@ class CentralStation_data(models.Model):
 
 class CentralStation_tanker(models.Model):
     _name="central.tanker"
-    name= fields.Char("Tanker No.")
+    name= fields.Many2one(string="Tanker No.",comodel_name="central.tanker_details")
     starting_pt= fields.Char(string="Source", default="Central Station")
     dest= fields.Many2one(string="Destination", comodel_name="central.substation")
     fuel_type= fields.Many2one(string="Fuel Type", comodel_name="central.data")
     vol=fields.Float("Volume Carried")
     status=fields.Selection([("dispatch", "Dispatched"),("on_way","On the Way"),("delivered","Delivered"),("late","Delayed")] ,string="Tanker Status", default="dispatch" ,required=True)
+    tanker_rec_id=fields.Integer(string="Tanker Record Id")
+    
+    # To generate tanker record id
+    @api.model
+    def create(self, vals_list):
+        res= super(CentralStation_tanker,self).create(vals_list)
+        res.tanker_rec_id=res.id
+        return res
 
+# for tanker details
+class CentralStation_tanker_details(models.Model):
+    _name="central.tanker_details"
+    name=fields.Char("Tanker NO.")
+    tanker_reg_no=fields.Char("Tanker Registration  NO.")
+    tanker_cap=fields.Float("Tanker Capacity")
+    driver_name= fields.Char("Driver Name")
+    driver_add= fields.Char("Driver Address")
+    driver_contact_no= fields.Integer("Driver Contact No.")
 
 #  SubStation Models
 class CentralStation_substation1(models.Model):
     _name= "central.substation1"
     name= fields.Char("Manager Name")
-
+    sub_add=fields.Char("Sub Station Address")
+    
 
 class CentralStation_substation1(models.Model):
     _name= "central.substation2"
     name= fields.Char("Manager Name")
+    sub_add=fields.Char("Sub Station Address")
 
 
 class CentralStation_substation1(models.Model):
     _name= "central.substation3"
     name= fields.Char("Manager Name")
+    sub_add=fields.Char("Sub Station Address")
